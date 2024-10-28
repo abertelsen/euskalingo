c1165fd037327c055f873f6ecf1e897ceea1c3bf
Álvaro Bertelsen <abertelsen@vicomtech.org>
2024-10-28 10:47:09 +0100

    Matching is now working.

--- tests/matching_test.py
+++ tests/matching_test.py
@@ -1,34 +1,13 @@
 import random
+import uuid
 
 import streamlit as st
 import streamlit_antd_components as sac
 
-def on_click():
-    if st.session_state.matches[0] is not None and st.session_state.matches[1] is not None and st.session_state.result is None:
-        word_left = buttons_left[st.session_state.matches[0]].label
-        word_right = buttons_right[st.session_state.matches[1]].label
-
-        if st.session_state.solution[word_left] == word_right:
-            st.session_state.result = True
-
-            st.session_state.disabled_left[st.session_state.matches[0]] = True
-            st.session_state.disabled_right[st.session_state.matches[1]] = True
-
-        else:
-            st.session_state.result = False
-
 st.set_page_config()
 
-if 'result' not in st.session_state:
-    st.session_state.result = None
-
-if st.session_state.result is not None:
-    st.toast('¡**{0}**!'.format('Correcto' if st.session_state.result else 'Incorrecto'))
-
-    st.session_state.matches[0] = None
-    st.session_state.matches[1] = None
-
-    st.session_state.result = None
+if 'finished' not in st.session_state:
+    st.session_state.finished = False 
 
 if 'solution' not in st.session_state:
     st.session_state.words_left = ['Donostia', 'Bilbo', 'Gasteiz', 'Iruñea', 'Baiona']
@@ -39,24 +18,34 @@ if 'solution' not in st.session_state:
     random.shuffle(st.session_state.words_left)
     random.shuffle(st.session_state.words_right)
 
-if 'matches' not in st.session_state:
-    st.session_state.matches = [None, None]
+if 'result' not in st.session_state:
+    st.session_state.result = None
 
-if 'disabled_left' not in st.session_state:
-    st.session_state.disabled_left = [False for x in range(5)]
+if st.session_state.result is not None:
+    st.toast('¡**{0}**!'.format('Correcto' if st.session_state.result else 'Incorrecto'))
 
-if 'disabled_right' not in st.session_state:
-    st.session_state.disabled_right = [False for x in range(5)]
+    st.session_state.result = None
+
+if 'matching_state' not in st.session_state:
+    st.session_state.matching_state = {
+        'matches': [None, None],
+        'disabled': [
+            [False for x in range(5)],
+            [False for x in range(5)]
+        ],
+        'chip_keys': [str(uuid.uuid4()), str(uuid.uuid4())]
+    }
 
 cols = st.columns(2)
 
-buttons_left = [sac.ChipItem(label=st.session_state.words_left[x], disabled=st.session_state.disabled_left[x]) for x in range(5)]
-buttons_right = [sac.ChipItem(label=st.session_state.words_right[x], disabled=st.session_state.disabled_right[x]) for x in range(5)]
+buttons_left = [sac.ChipItem(label=st.session_state.words_left[x], disabled=st.session_state.matching_state['disabled'][0][x]) for x in range(5)]
+buttons_right = [sac.ChipItem(label=st.session_state.words_right[x], disabled=st.session_state.matching_state['disabled'][1][x]) for x in range(5)]
 
 for (k,v) in enumerate([buttons_left, buttons_right]):
     with cols[k]:
-        st.session_state.matches[k] = sac.chip(items=v,
-            index=st.session_state.matches[k],
+        st.session_state.matching_state['matches'][k] = sac.chip(
+            items=v,
+            index=None,
             label='',
             color='grape',
             direction='vertical',
@@ -65,5 +54,29 @@ for (k,v) in enumerate([buttons_left, buttons_right]):
             radius='md',
             variant='outline',
             align='center',
-            on_change=on_click,
-            key='matchchips_{0}'.format('left' if k==0 else 'right'))
+            key=st.session_state.matching_state['chip_keys'][k]
+        )
+
+if st.session_state.matching_state['matches'][0] is not None and st.session_state.matching_state['matches'][1] is not None:
+    word_left = buttons_left[st.session_state.matching_state['matches'][0]].label
+    word_right = buttons_right[st.session_state.matching_state['matches'][1]].label
+
+    if st.session_state.solution[word_left] == word_right:
+        st.session_state.result = True
+
+        st.session_state.matching_state['disabled'][0][st.session_state.matching_state['matches'][0]] = True
+        st.session_state.matching_state['disabled'][1][st.session_state.matching_state['matches'][1]] = True
+
+    else:
+        st.session_state.result = False
+
+    st.session_state.matching_state['matches'][0] = None
+    st.session_state.matching_state['matches'][1] = None
+
+    st.session_state.matching_state['chip_keys'] = [str(uuid.uuid4()), str(uuid.uuid4())]
+
+if all(st.session_state.matching_state['disabled'][0]) and all(st.session_state.matching_state['disabled'][1]):
+    st.session_state.finished = True
+
+if st.session_state.finished:
+    st.markdown('¡Ejercicio finalizado!')
