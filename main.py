7f26d74bc467bb1c48c3909cf69b710fe6d6efa9
Álvaro Bertelsen <abertelsen@vicomtech.org>
2024-11-13 17:21:22 +0100

    Unable to inject code. Reverting...

--- main.py
+++ main.py
@@ -1,41 +1,28 @@
-import pathlib
+# import pathlib
 # import shutil
 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import streamlit as st
 
-def inject_ga():
-    # GA_ID = "google_analytics"
-    # GA_JS = """
-    # <!-- Global site tag (gtag.js) - Google Analytics -->
-    # <script async src="https://www.googletagmanager.com/gtag/js?id=G-**********"></script>
-    # <script>
-    #     window.dataLayer = window.dataLayer || [];
-    #     function gtag(){dataLayer.push(arguments);}
-    #     gtag('js', new Date());
+# def inject_ga():
+#     GA_ID = "google_adsense"
+#     GA_META = '<meta name="google-adsense-account" content="ca-pub-xxxxxxxxxxxx9488">'
 
-    #     gtag('config', 'G-**********');
-    # </script>
-    # """
+#     # Insert the script in the head tag of the static template inside your virtual
+#     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+#     # logging.info(f'editing {index_path}')
+#     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+#     if not soup.find(id=GA_ID): 
+#         bck_index = index_path.with_suffix('.bck')
+#         if bck_index.exists():
+#             shutil.copy(bck_index, index_path)  # FAIL PermissionDenied
+#         else:
+#             shutil.copy(index_path, bck_index)  # FAIL PermissionDenied
+#         html = str(soup)
+#         new_html = html.replace('<head>', '<head>\n' + GA_META)
+#         index_path.write_text(new_html)  # FAIL PermissionDenied
 
-    GA_ID = "google_adsense"
-    GA_META = '<meta name="google-adsense-account" content="ca-pub-5145093140039488">'
-
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    # logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        # bck_index = index_path.with_suffix('.bck')
-        # if bck_index.exists():
-        #     shutil.copy(bck_index, index_path)  
-        # else:
-        #     shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_META)
-        index_path.write_text(new_html)
-
-inject_ga()
+# inject_ga()
 
 st.set_page_config(page_title="Euskera", layout="wide")
 
